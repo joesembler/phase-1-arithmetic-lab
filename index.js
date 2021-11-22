1e5dc11465f143b50e55f276ca659dec17d74d60
Joe Sembler <joeysembler@gmail.com>
2021-11-22 12:51:21 -0500

    commit tree 284d171a498160389a70855a037f037ae495b027

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// Joe Sembler
+// Flatiron School
+// Phase 1: Arithmetic Lab
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = math.floor(math.random() * 10) + 1;
